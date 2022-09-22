f24bd737b7fbf9a3863a8c7e8c2fb6c1f536f686
tbeatrice <105853421+tbeatrice@users.noreply.github.com>
2022-09-22 12:21:40 +0300

    refactor: :recycle: rename lib folder to utilities

--- components/GoogleLoginCard.tsx
+++ components/GoogleLoginCard.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Router, { useRouter } from "next/router";
 import GoogleLogin from "react-google-login";
-import fetchJwt from "../lib/fetchJwt";
+import fetchJwt from "../utilities/fetchJwt";
 import { authStore } from "../store/authStore";
 
 async function handleSuccess(response: any) {
--- components/RequireAuth.tsx
+++ components/RequireAuth.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import checkValidJwt from "../lib/checkValidJwt";
+import checkValidJwt from "../utilities/checkValidJwt";
 
 interface Props {
   children: any;
