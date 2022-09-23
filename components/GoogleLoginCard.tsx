e99ad72b1d233b147334aecdaa68769bc206fc5e
tbeatrice <105853421+tbeatrice@users.noreply.github.com>
2022-09-23 12:38:11 +0300

    refactor: :recycle: code

--- components/GoogleLoginCard.tsx
+++ components/GoogleLoginCard.tsx
@@ -1,8 +1,6 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import Router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import GoogleLogin from "react-google-login";
 import fetchJwt from "../utilities/fetchJwt";
-import { authStore } from "../store/authStore";
 
 async function handleSuccess(response: any) {
   const tokenId = response.tokenId as string;
@@ -11,8 +9,6 @@ async function handleSuccess(response: any) {
   console.log(jwt);
   // TODO: if jwt is undefined do something
   localStorage.setItem("jwt", jwt || "undefined");
-
-  // Router.push("dashboard");
 }
 
 function handleFailure(err: any) {
